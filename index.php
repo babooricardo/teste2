f0e183bc3643b7665aa3ae174128ce04a80b0194
Ricardo Baboo <122166261+babooricardo@users.noreply.github.com>
2023-01-12 20:10:29 -0300

    alt pós for e function

--- index.php
+++ index.php
@@ -7,16 +7,22 @@
 
 /* fechando a conexão */
 
+
+
+/* Fazendo um giro */
 for ($contador=0; $contador < 80; $contador++) { 
     printar($contador);
 }
 
 
-
+/*Função */
 function printar($contador){
     echo $contador
 } 
      
+
+
+
      
 
 ?>
