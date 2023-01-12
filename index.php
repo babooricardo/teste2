a399d6f2947efc158b4d7e859f03f35d740feea6
Ricardo Baboo <122166261+babooricardo@users.noreply.github.com>
2023-01-12 20:16:37 -0300

    restaurando

--- index.php
+++ index.php
@@ -7,22 +7,16 @@
 
 /* fechando a conexão */
 
-
-
-/* Fazendo um giro */
 for ($contador=0; $contador < 80; $contador++) { 
     printar($contador);
 }
 
 
-/*Função */
+
 function printar($contador){
     echo $contador
 } 
      
-
-
-
      
 
 ?>
